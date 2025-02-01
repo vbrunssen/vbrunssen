5cbd08dcb6faa232d9f71c9ee3612ef39de9d80a
vbrunssen <166144919+vbrunssen@users.noreply.github.com>
2025-02-01 04:18:54 +0000

    Fix typo

---
+++ Test/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("HelloWorld!");
+        System.out.println("This is an additional string to print");
+    }
+}
